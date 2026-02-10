f274e5684262ff2622048988cf1f7842394254e8
Samikshya Shrestha <sami.college098@gmail.com>
2026-02-10 21:08:38 +0545

    singleitem-js

---
+++ js/singleitem.js
@@ -0,0 +1,32 @@
+function createSingleItem(item) {
+  var $div = $('<div class="single-item"></div>');
+
+  var $checkbox = $('<input type="checkbox" />')
+    .prop("checked", item.completed)
+    .on("change", function () {
+      editCompleted(item.id);
+    });
+
+  var $text = $("<p></p>")
+    .text(item.name)
+    .css("text-decoration", item.completed ? "line-through" : "none");
+
+  var $editBtn = $(`
+    <button class="btn icon-btn edit-btn" type="button">
+      <i class="fa-regular fa-pen-to-square"></i>
+    </button>
+  `).on("click", function () {
+    editItem(item.id);
+  });
+
+  var $deleteBtn = $(`
+    <button class="btn icon-btn remove-btn" type="button">
+      <i class="fa-regular fa-trash-can"></i>
+    </button>
+  `).on("click", function () {
+    deleteItem(item.id);
+  });
+
+  $div.append($checkbox, $text, $editBtn, $deleteBtn);
+  return $div;
+}
